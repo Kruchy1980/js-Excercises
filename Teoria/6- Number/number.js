2ac065fdc4fbaa1ec6bc3875c39d8c8220166b72
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-11-04 19:13:29 +0100

    Initial of number detailed information

--- Teoria/6- Number/number.js
+++ Teoria/6- Number/number.js
@@ -1,1 +1,6 @@
-// Type number :
+// Type number : is a straight type of data:
+
+const integerExample = 100;
+const decimalExample = 100.212;
+typeof integerExample; // Intiger number
+typeof decimalExample;
