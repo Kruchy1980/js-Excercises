3f4100fe7573e0561b4f5c49c292d47b4985ccaa
Kruchy1980 <jakub.kruszynski@vp.pl>
2020-01-30 16:51:08 +0100

    Excercise20 paper stone scissors game finished

--- Zadania/Zadanie20/zadanie20.js
+++ Zadania/Zadanie20/zadanie20.js
@@ -4,8 +4,24 @@ const playBtn = document.querySelector('.play');
 // place for communicate to user when he wom't choose the weapon
 const info = document.querySelector('.choose');
 // result information
-let winner = document.querySelector('.result');
-// General game data object
+const winner = document.querySelector('.result');
+// General game informations 
+//Player choice
+const choicePlayerInfo = document.querySelector('[data-summary="your-choice"]');
+// Ai Choice
+const choiceAiInfo = document.querySelector('[data-summary="ai-choice');
+// now we can declare the variables of particular game quantity
+// 1,Total Games
+let totalGames = document.querySelector('.gameQuantity span');
+// 2. Wins
+let winResult = document.querySelector('.wins span');
+// 3.Looses
+let lossResult = document.querySelector('.looses span');
+// 4.Draws
+let drawResult = document.querySelector('.draws span');
+// Who win if we want to use it to give another information to result if smth
+let whoWin = document.querySelector('[data-summary="who-win"]');
+// General Game Object
 const gameSummary = {
     numbers: 0,
     wins: 0,
@@ -71,12 +87,79 @@ function checkResult(player, ai) {
     winner.style.fontSize = '3em';
     winner.style.color = '#0f0';
     // first condition if there is draw
-    if (player === ai) return winner.textContent = 'Remis';
+    if (player === ai) {
+        winner.style.fontWeight = 'bold';
+        winner.style.color = 'grey';
+        return winner.textContent = 'Remis';
+    }
     //user winner condition
-    else if ((player === 'paper' && ai === 'stone') || (player === 'stoe' && ai === 'scissors') || (player === 'scissons' && ai === 'paper')) return winner.textContent = 'Player has won!';
+    else if ((player === 'paper' && ai === 'stone') || (player === 'stoe' && ai === 'scissors') || (player === 'scissons' && ai === 'paper')) {
+        // some  styling of result
+        winner.style.fontWeight = 'bold';
+        winner.style.color = '#00ff00';
+        return winner.textContent = 'Win!';
+    }
     // Ai has won
-    else return winner.textContent = 'Computer has won!';
+    else {
+        winner.style.fontWeight = 'bold';
+        winner.style.color = '#ff0000';
+        return winner.textContent = 'Won!';
+    }
 }
+//4 Function which will publish the statistic of game and choices of 
+function publishResult(player, ai, result) {
+    // first we can check if the function is recognized by steering function 
+    console.log(this); //we can see it is the element of dom which interests us at present
+    // Now we can get element where we will display interested us informations
+    // secod solution is to use the getters directly in the function separately or as a variable.
+    // // //Player choice - variable
+    // const choicePlayerInfo = document.querySelector('[data-summary="your-choice"]');
+    // directly
+    //document.querySelector'data-summary="your-choice"]').textContetn = player;
+    // now we can set the result
+    choicePlayerInfo.textContent = player;
+    // some styles to our player choice information
+    choicePlayerInfo.style.fontWeight = 'bold';
+    choicePlayerInfo.style.color = '#00ff00';
+
+    // // Ai Choice - also two solutions can be ued
+    // const choiceAiInfo = document.querySelector('[data-summary="ai-choice');
+    //now the ai result
+    choiceAiInfo.textContent = ai;
+    // Some styles to ai info display
+    choiceAiInfo.style.fontWeight = 'bold';
+    choiceAiInfo.style.color = '#ff0000';
+    // Now we can directly from object take a total number of games because theyare not dependenc od the result so we can add smth like that - on top define the variables
+    // Now the time for use the gameSummary
+    totalGames.textContent = ++gameSummary.numbers;
+    // now time for all the rest it will be under if statement
+    // draw
+    if (result === 'Remis') {
+        whoWin.textContent = 'Wyjątek od każdej reguły - Remis';
+        return drawResult.textContent = ++gameSummary.draws;
+        // win
+    } else if (result === 'Win!') {
+        whoWin.textContent = 'Gracz jak zwykle górą:)';
+        return winResult.textContent = ++gameSummary.wins;
+        // here we can use data-summary to tell who has won
+        //loose
+    } else if (result === 'Won!') {
+        whoWin.textContent = 'Tym razem dałeś fory komputerowi';
+        return lossResult.textContent = ++gameSummary.losses;
+    }
+
+}
+// 5. Player choice re-set function
+function choiceReset() {
+    // reset the element choose
+    // here we will use the direct declaration and getter because our choosen element won't change in a time.
+    document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = '';
+    // now we need remove the weapon from the memory
+    game.playerHand = '';
+    // and the ai weapon - no necessary just for rules
+    game.aiHand = '';
+};
+
 
 // 2. Main steering function - this is a function which connects all the smaller functions (including the AI choice) together to work properly - for clicking on button Lets play - will be written as declarative function to show that it is good code specially for game which are using objects
 
@@ -102,6 +185,11 @@ function steeringFunc() {
     game.aiHand = computerChoice();
     // 3. here we can declare variable of result - with two arguments - player choice and ai choice - we need to set the arguments as a parameters of function
     const gameResult = checkResult(game.playerHand, game.aiHand);
+    //4. Results publicity function call
+    publishResult(game.playerHand, game.aiHand, gameResult);
+    //5. Player choice reset function
+    choiceReset();
+
 }
 
 // EventListeners
