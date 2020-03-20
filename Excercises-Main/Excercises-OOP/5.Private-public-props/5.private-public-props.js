87b4d7b6453a0588efbf2c0b9bcdc5bc04119788
Kruchy1980 <jakub.kruszynski@vp.pl>
2020-03-20 17:22:49 +0100

    Initial of Privacy and publicity in OOP

---
+++ Excercises-Main/Excercises-OOP/5.Private-public-props/5.private-public-props.js
@@ -0,0 +1,16 @@
+// PRIVATE AND PUBLIC PROPERTIES, CONVENYION, CLOSURES
+// 1. Private and Public Properties
+
+
+
+// 2. Hiding Data.
+
+
+
+// 3. Convention.
+
+
+
+// 4. Closures usage.
+
+// BONUS EXAMPLES
